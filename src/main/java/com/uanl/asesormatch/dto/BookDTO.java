5a43ec913aecdd52532b7116571cd58cfe198c53
Gerardo Villarreal <38843413+geravillarreal@users.noreply.github.com>
2025-07-14 23:13:44 -0600

    Add book id support

--- src/main/java/com/uanl/asesormatch/dto/BookDTO.java
+++ src/main/java/com/uanl/asesormatch/dto/BookDTO.java
@@ -1,8 +1,17 @@
 package com.uanl.asesormatch.dto;
 
 public class BookDTO {
-	private String title;
-	private String description;
+        private Long id;
+        private String title;
+        private String description;
+
+        public Long getId() {
+                return id;
+        }
+
+        public void setId(Long id) {
+                this.id = id;
+        }
 
 	public String getTitle() {
 		return title;
