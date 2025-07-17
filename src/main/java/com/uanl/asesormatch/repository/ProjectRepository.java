f627085d9cee9f12eedfddf52296e124a1d1318d
Gerardo Villarreal <38843413+geravillarreal@users.noreply.github.com>
2025-07-16 23:11:06 -0600

    Update advisor dashboard to show completed projects

--- src/main/java/com/uanl/asesormatch/repository/ProjectRepository.java
+++ src/main/java/com/uanl/asesormatch/repository/ProjectRepository.java
@@ -6,9 +6,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.uanl.asesormatch.entity.Project;
 import com.uanl.asesormatch.entity.User;
+import com.uanl.asesormatch.enums.ProjectStatus;
 
 public interface ProjectRepository extends JpaRepository<Project, Long> {
     List<Project> findByAdvisor(User advisor);
 
-	List<Project> findByStudent(User user);
+    List<Project> findByStudent(User user);
+
+    long countByAdvisorAndStatus(User advisor, ProjectStatus status);
 }
